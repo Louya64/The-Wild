abc23b1108b39e5fbb3a8aecf0510a7e21ec32c6
Jimmy <jimmy.ganci@gmail.com>
2021-10-06 09:14:16 +0200

    finished resto desktop

--- js/resto.js
+++ js/resto.js
@@ -24,12 +24,18 @@ if (mealsMeat) {
           buttonsMeat[0].classList.remove("active");
           buttonsMeat[2].classList.remove("active");
           mealsMeatList[i].classList.remove("active");
-          mealsVegeList[i].classList.remove("active");
+          if (mealsVegeList[i]) {
+            mealsVegeList[i].classList.remove("active");
+            mealsVegeList[i].style.transition = `${(i + 0.2) / 5}s ease-in`;
+          }
           mealsMeatList[i].style.transition = `${(i + 0.2) / 5}s ease-in`;
-          mealsVegeList[i].style.transition = `${(i + 0.2) / 5}s ease-in`;
           mealsFishList[i].style.transition = `1s ease-in-out`;
-          // mealsFishList[i].style.transitionDelay = `1s`;
+          document.getElementById(
+            "meals__image"
+          ).src = `assets/resto/meals/12.jpg`;
           mealsFishList[i].classList.add("active");
+          document.getElementById("title_meal").innerHTML =
+            mealsFishList[1].textContent;
         } else if (j === 2) {
           mealsFish.style.zIndex = 0;
           mealsMeat.style.zIndex = 0;
@@ -41,8 +47,15 @@ if (mealsMeat) {
           mealsFishList[i].classList.remove("active");
           mealsFishList[i].style.transition = `${(i + 0.2) / 5}s ease-in`;
           mealsMeatList[i].style.transition = `${(i + 0.2) / 5}s ease-in`;
-          mealsVegeList[i].style.transition = `1s ease-in-out`;
-          mealsVegeList[i].classList.add("active");
+          if (mealsVegeList[i]) {
+            mealsVegeList[i].style.transition = `1s ease-in-out`;
+            mealsVegeList[i].classList.add("active");
+          }
+          document.getElementById(
+            "meals__image"
+          ).src = `assets/resto/meals/23.jpg`;
+          document.getElementById("title_meal").innerHTML =
+            mealsVegeList[1].textContent;
         } else {
           mealsFish.style.zIndex = 0;
           mealsMeat.style.zIndex = 7;
@@ -53,17 +66,63 @@ if (mealsMeat) {
           buttonsMeat[2].classList.remove("active");
           mealsMeatList[i].style.transition = `1s ease-in-out`;
           mealsFishList[i].classList.remove("active");
-          mealsVegeList[i].classList.remove("active");
+          if (mealsVegeList[i]) {
+            mealsVegeList[i].classList.remove("active");
+            mealsVegeList[i].style.transition = `${(i + 0.2) / 5}s ease-in`;
+          }
           mealsFishList[i].style.transition = `${(i + 0.2) / 5}s ease-in`;
-          mealsVegeList[i].style.transition = `${(i + 0.2) / 5}s ease-in`;
+
+          document.getElementById(
+            "meals__image"
+          ).src = `assets/resto/meals/1.jpg`;
+          document.getElementById("title_meal").innerHTML =
+            mealsMeatList[1].textContent;
         }
       });
     }
   }
   const mealInfos = document.querySelectorAll(".meals__main--text__grid > li");
   for (let i = 0; i < mealInfos.length; i++) {
-    mealInfos[i].addEventListener("click", () => {
-      console.log("test");
+    mealInfos[i].addEventListener("click", (e) => {
+      document.getElementById(
+        "meals__image"
+      ).src = `assets/resto/meals/${i}.jpg`;
+      document.getElementById("title_meal").innerHTML = e.target.textContent;
+    });
+    mealInfos[i].addEventListener("mouseover", () => {
+      mealInfos[i].style.color = "white";
+    });
+    mealInfos[i].addEventListener("mouseout", () => {
+      mealInfos[i].style.color = "black";
     });
   }
+}
+// reveal titre accompagenement plats
+const titleSide = document.querySelectorAll(".side__grid--plate");
+for (let i = 0; i < titleSide.length; i++) {
+  titleSide[i].addEventListener("mouseover", () => {
+    const h3 = titleSide[i].childNodes;
+    for (let j = 0; j < h3.length; j++) {
+      h3[j].style.opacity = "1";
+      h3[j].style.height = "18%";
+    }
+  });
+  titleSide[i].addEventListener("mouseout", () => {
+    const h3 = titleSide[i].childNodes;
+    for (let j = 0; j < h3.length; j++) {
+      h3[j].style.opacity = "0";
+      h3[j].style.height = "0";
+    }
+  });
+}
+// --------Hover desserts------------------
+const gridText = document.querySelectorAll(".dessert__grid--text");
+
+for (let i = 0; i < gridText.length; i++) {
+  gridText[i].addEventListener("mouseover", () => {
+    gridText[i].style.opacity = "1";
+  });
+  gridText[i].addEventListener("mouseout", () => {
+    gridText[i].style.opacity = "0";
+  });
 }
